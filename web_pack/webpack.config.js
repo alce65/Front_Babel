745101fdfd51127a0bcc5f8e6a2f7dc22a0a1ed8
alce65 <alce65@hotmail.es>
2018-04-02 13:13:29 +0200

    webpack dev server

--- web_pack/webpack.config.js
+++ web_pack/webpack.config.js
@@ -1,36 +1,36 @@
-const path = require('path');
+const path = require("path");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 const extractSass = new ExtractTextPlugin({
-    filename: "style.css",
-    //disable: process.env.NODE_ENV === "development"
+  filename: "style.css"
+  //disable: process.env.NODE_ENV === "development"
 });
 
-
 module.exports = {
-    entry: './app.js',
-    output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js'
-    },
-    module:{
-        rules:[
-                {
-                    test:/\.(s*)css$/,
-                    use: extractSass.extract({
-                      use: [
-                        {loader: "css-loader"},
-                        {loader: "sass-loader"}
-                      ],
-                      // use style-loader in development
-                      fallback: "style-loader"
-                    })
-                 }
-         ]
-      },
-      plugins : [
-        extractSass
-      ],
-  mode: 'development'
-  // mode: 'production'
-  };
+  entry: "./app.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(s*)css$/,
+        use: extractSass.extract({
+          use: [{ loader: "css-loader" }, { loader: "sass-loader" }],
+          // use style-loader in development
+          fallback: "style-loader"
+        })
+      }
+    ]
+  },
+  plugins: [extractSass],
+  mode: "development",
+  // mode: 'production',
+  devServer: {
+    host: '127.0.0.1',
+    port: 8085,
+    inline: true,
+    contentBase: path.join(__dirname, "dist")
+  }
+};
