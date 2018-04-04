2ea7f42b7dd77d7645c2b0631eca8a430aeeb028
alce65 <alce65@hotmail.es>
2018-04-04 13:52:19 +0200

    Cambio server ws

--- api.connectivity/server_ws/index.js
+++ api.connectivity/server_ws/index.js
@@ -1,8 +1,13 @@
 var WebSocketServer = require( 'ws' ).Server;
 var wss = new WebSocketServer({port: 8085});
 wss.on('connection', function(ws) {
-	ws.on ('message', function(message) {
-		console.log(' received: %s ', message) ;
-	});
+    ws.on('message', (message) => {
+        console.log(' received: %s ', message) ;
+        setTimeout(() => {
+            console.log(' resending: %s ', message) 
+            ws.send(`Hola ${message}`)
+        }, 500);
+    });
 	ws.send('something') ;
 });
+
